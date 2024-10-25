4151ecafd50452129f874d9eba3afffaa541da21
Martin Federico Moran <martinfmoran@icloud.com>
2024-10-25 19:49:46 -0300

    cambios de validacion y comentarios finales

--- pre_entrega1.py
+++ pre_entrega1.py
@@ -4,23 +4,36 @@ Crear un menú interactivo utilizando bucles while y condicionales if-elif-else:
 El menú debe permitir al usuario seleccionar entre diferentes opciones relacionadas con la gestión de productos. 
 Entre las opciones, deben incluirse: agregar productos al inventario y mostrar los productos registrados. """
 
+# Inicializacion de la variable
 choose = 0
 
-products_list = []
+# Menu inicial
+print("\n=============== INVENTARIO ===============")
+print("1. Agregar prodcutos al inventario")
+print("2. Mostrar productos registrados")
+print("3. Salir")
+print("==========================================\n")
 
-
+# Ciclo while se valida que la eleccion del usuario sea distinta de salir (3)
 while choose != 3:
-    print("\n=============== INVENTARIO ===============")
-    print("1. Agregar prodcutos al inventario")
-    print("2. Mostrar productos registrados")
-    print("3. Salir")
-    print("==========================================\n")
-    
     choose = int(input("Elija una opcion del menu (1, 2 o 3): "))
-
+# El if verifica que selecciona en el menu el numero 1 y modifica el valor de la variable para salir del ciclo y sistema.
     if choose == 1:
         print(f'\nElegiste la opcion {choose} "Agregar prodcutos al inventario"\n')
+        choose +=2
+# Elif verifica que selecciona en el menu el numero 2 y modifica el valor de la variable para salir del ciclo y sistema.
     elif choose == 2:
         print(f'\nElegiste la opcion {choose} "Mostrar productos registrados"\n')
-    else:
+        choose +=1
+# Elif verifica que selecciona en el menu el numero 3, dando un mensaje de la salida del ciclo y sistema.
+    elif choose == 3:
         print(f'\nElegiste opcion {choose} para salir del sistema, hasta luego!\n')
+# El else se usa para validar que ingresa un valor correcto y se ofrece nuevamente el menu.
+    else:
+        print(f'\nEl numero {choose} no es una opcion correcta, intenta nuevamente!\n')
+        
+        print("=============== INVENTARIO ===============")
+        print("1. Agregar prodcutos al inventario")
+        print("2. Mostrar productos registrados")
+        print("3. Salir")
+        print("==========================================\n")
